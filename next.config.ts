08403a20b765d88e3a10510c2a8979d950ab8691
LamGC <lam827@lamgc.net>
2025-05-06 03:15:34 +0000

    初步增加部分 Avatar 来源白名单，以支持开箱即用的 URL 形式 Avatar。

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,86 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/*',
+      },
+
+      // Tips: 下面的配置都没有经过完整测试，可能会有问题。
+      // Gravatar（允许任意查询参数、深层路径）
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+        port: '',
+        pathname: '/avatar/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        port: '',
+        pathname: '/avatar/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gravatar.com',
+        port: '',
+        pathname: '/avatar/**',
+      },
+      // Twitter（允许任意查询参数）
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+        pathname: '/profile_images/*/*',
+      },
+      // Facebook（允许任意查询参数）
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',
+        port: '',
+        pathname: '/**/picture',
+      },
+      // Google 用户头像（允许任意查询参数、任意深度路径）
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Discord（允许任意查询参数）
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/avatars/*/*',
+      },
+      // Slack（任意子域，任意深度路径）
+      {
+        protocol: 'https',
+        hostname: '**.slack-edge.com',
+        port: '',
+        pathname: '/avatars/**',
+      },
+      // LinkedIn（允许任意查询参数、深层路径）
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/mpr/mpr/**',
+      },
+      // Instagram（多级子域，任意深度路径）
+      {
+        protocol: 'https',
+        hostname: 'instagram.**.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
